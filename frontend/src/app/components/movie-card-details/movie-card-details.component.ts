755dc54fb7d2c662daf314a233ddf0b559f5c336
javixlive <javixlive@hotmail.com>
2024-08-25 12:07:19 -0600

    changes to favorites

--- frontend/src/app/components/movie-card-details/movie-card-details.component.ts
+++ frontend/src/app/components/movie-card-details/movie-card-details.component.ts
@@ -1,6 +1,8 @@
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { catchError, tap } from "rxjs/operators";
 import { combineLatest } from 'rxjs';
 
 import { MovieService } from '../../shared/services/movie.service';
@@ -10,6 +12,10 @@ import { MovieDetails } from '../../interface/details.interface';
 
 import { TitlesResponse, Movie } from "../../interface/titles.interface";
 
+import { environment } from "../../environment/environment";
+
+
+const urlDb = environment.URLDB
 
 @Component({
   selector: 'app-movie-card-details',
@@ -23,7 +29,9 @@ export class MovieCardDetailsComponent implements OnInit {
 
   movie?:MovieDetails;
   cast : Cast[] = [];
-  constructor(private activatedRoute:ActivatedRoute, private movieSvc:MovieService){}
+  constructor(private activatedRoute:ActivatedRoute, private movieSvc:MovieService, private http: HttpClient){}
+
+  private isLocalStorageAvailable = typeof localStorage !== 'undefined';
 
   //combine all the data from details and credits of the movie
   ngOnInit() {
@@ -48,6 +56,34 @@ export class MovieCardDetailsComponent implements OnInit {
     })
 
   }
+
+  addToFavorites() {
+    const {id} = this.activatedRoute.snapshot.params;
+    var movieId = JSON.stringify(id)
+
+    if(this.isLocalStorageAvailable) {
+        const userData: {
+          userEmail: string;
+          userId: string;
+          _token: string;
+          _tokenExpirationDate: string;
+        } = JSON.parse(localStorage.getItem('userData'));
+
+        //TODO: work on adding favorites
+        return this.http.post(
+          `${urlDb}api/Favorites`,
+          {
+            userId: userData.userId,
+            movieId: movieId
+          }
+        ).pipe(
+          tap(resData => {
+            console.log(resData)
+          }))
+      }
+      return "Error"
+  }
+
   //get avg stars
   getStars(voteAverage:number) {
     const starsCount = Math.floor(voteAverage);
